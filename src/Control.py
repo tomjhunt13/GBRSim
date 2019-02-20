6a00283c687a7fff4fa9c6c5d27e7ab0f4dfd58e
Thomas Hunt <tomjhunt13@gmail.com>
2019-02-20 15:20:57 +0000

    Added control strategies

---
+++ src/Control.py
@@ -0,0 +1,60 @@
+class ConstantPower:
+    def demand(self, *args):
+        return 1
+
+class CutoffSpeed:
+    def __init__(self, cutoff_speed):
+        self.cutoff_speed = cutoff_speed
+
+    def demand(self, *args):
+
+        # Unpack args
+        V = args[0]
+        theta = args[1]
+
+        if V > self.power_cutoff_speed or theta < 0:
+            throttle_demand = 0
+
+        else:
+            throttle_demand = 1
+
+        return throttle_demand
+
+class BurnAndCoast_Velocity:
+    def __init__(self, min_vel, max_vel):
+        self.min_vel = min_vel
+        self.max_vel = max_vel
+
+        self.previous_throttle = 0
+
+    def demand(self, *args):
+
+        # Unpack args
+        V = args[0]
+        theta = args[1]
+
+        previous_throttle = int(self.previous_throttle)
+
+        if theta < 0:
+            self.previous_throttle = 0
+            return 0
+
+        if V < self.min_vel:
+            self.previous_throttle = 1
+            return 1
+
+        if V > self.max_vel:
+            self.previous_throttle = 0
+            return 0
+
+
+        # At this point min_vel < V < max_vel
+        if previous_throttle == 1:
+            # Must still be burning
+            self.previous_throttle = 1
+            return 1
+
+        # Else must be coasting
+        self.previous_throttle = 0
+        return 0
+
