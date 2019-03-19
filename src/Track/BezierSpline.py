fbec27828a273e8ae5f7f2bccbdd10a2b69eed1c
Thomas Hunt <tomjhunt13@gmail.com>
2019-03-19 15:22:04 +0000

    Added first derivative calculation  and test

--- src/Track/BezierSpline.py
+++ src/Track/BezierSpline.py
@@ -6,20 +6,20 @@ class CubicBezier:
         self.knots = knots
         self.control_points = control_points
 
-    def draw_coordinates(self, num_arc_points=20):
+    def draw_coordinates(self, num_segments=20):
         """
         Calculate 3D coordinates to draw segment
-        :param num_arc_points: Number of points used to discretise curve
+        :param num_segments: Number of points used to discretise curve
         :return: tuple: x_coordinates, y_coordinates, z_coordinates
         """
 
-        x = [None] * num_arc_points
-        y = [None] * num_arc_points
-        z = [None] * num_arc_points
+        x = [None] * (num_segments + 1)
+        y = [None] * (num_segments + 1)
+        z = [None] * (num_segments + 1)
 
-        for i in range(num_arc_points):
+        for i in range(num_segments + 1):
 
-            t = float(i) / num_arc_points
+            t = float(i) / num_segments
             P = self.position(t)
 
             x[i] = P[0]
@@ -38,13 +38,15 @@ class CubicBezier:
 
         pass
 
-    def direction(self, segment, lambda_param):
+    def direction(self, lambda_param):
         """
         For a given point on the track, get the direction of travel a unit vector
         :param segment: integer - Index of segment of track to use
         :param lambda_param: float between 0 and 1 - value of parameter lambda to get direction of
         :return: 3 element list - Direction vector of track
         """
+
+
 
         pass
 
@@ -76,6 +78,34 @@ class CubicBezier:
         P2_contribution = np.multiply(3 * one_minus_t * t_squared, P2)
         P3_contribution = np.multiply(t_cubed, P3)
 
-        return np.add(np.add(P0_contribution, P1_contribution), np.add(P2_contribution, P3_contribution))
+        return list(np.add(np.add(P0_contribution, P1_contribution), np.add(P2_contribution, P3_contribution)))
+
+    def derivative(self, t):
+        """
+        For a value of parameter t, compute the derivative wrt t
+        :param t: float between 0 and 1 - value of t to get derivative at
+        :return: 3 element list - Derivative vector
+        """
+
+        # Unpack knot vectors
+        P0 = self.knots[0]
+        P1 = self.control_points[0]
+        P2 = self.control_points[1]
+        P3 = self.knots[1]
+
+        # Calculate t parameters
+        t_squared = t * t
+        one_minus_t = (1 - t)
+        one_minus_t_squared = one_minus_t * one_minus_t
+
+
+        # Get contributions
+        P0_contribution = np.multiply(-3 * one_minus_t_squared, P0)
+        P1_contribution = np.multiply(3 * (1 - 4 * t + 3 * t_squared), P1)
+        P2_contribution = np.multiply(3 * (2 * t - 3 * t_squared), P2)
+        P3_contribution = np.multiply(3 * t_squared, P3)
+
+        return list(np.add(np.add(P0_contribution, P1_contribution), np.add(P2_contribution, P3_contribution)))
+
 
 
--- src/Track/Tests/Test_BezierSpline.py
+++ src/Track/Tests/Test_BezierSpline.py
@@ -5,7 +5,7 @@ from src.Track.BezierSpline import *
 
 class TestBezierSpline(unittest.TestCase):
 
-    def test_EndsMatchControlPoints(self):
+    def test_EndsMatchKnots(self):
 
         # Flat test
         knots = [[0, 0, 0], [0, 1, 0]]
@@ -26,6 +26,49 @@ class TestBezierSpline(unittest.TestCase):
 
             for i in range(3):
                 self.assertEqual(P[i], b.knots[t][i])
+
+    def test_Derivatives(self):
+
+        # Flat test
+        knots = [[0, 0, 0], [3, 0, 0]]
+        control_points = [[1, 0, 0], [2, 0, 0]]
+
+        b = CubicBezier(knots, control_points)
+
+        computed_derivative = b.derivative(0.5)
+        expected_derivative = [3, 0, 0]
+
+
+        for i in range(3):
+            self.assertEqual(computed_derivative[i], expected_derivative[i])
+
+        # Angled 1
+        knots = [[0, 0, 0], [3, 0, 3]]
+        control_points = [[1, 0, 1], [2, 0, 2]]
+
+        b = CubicBezier(knots, control_points)
+
+        computed_derivative = b.derivative(0.5)
+        expected_derivative = [3, 0, 3]
+
+        for i in range(3):
+            self.assertEqual(computed_derivative[i], expected_derivative[i])
+
+        # Angled 2
+        knots = [[0, 0, 0], [3, 3, 3]]
+        control_points = [[1, 1, 1], [2, 2, 2]]
+
+        b = CubicBezier(knots, control_points)
+
+        computed_derivative = b.derivative(0.5)
+        expected_derivative = [3, 3, 3]
+
+        for i in range(3):
+            self.assertEqual(computed_derivative[i], expected_derivative[i])
+
+
+
+
 
 
 
