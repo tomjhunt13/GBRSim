a40e378ddc0d4b4ab07767851defcd17e806e233
Thomas Hunt <tomjhunt13@gmail.com>
2019-05-09 11:04:08 +0100

    Further tests

--- src/Track/tests/test_segment_lambda_from_arc_length.py
+++ src/Track/tests/test_segment_lambda_from_arc_length.py
@@ -1,12 +1,11 @@
 import unittest
-import numpy as np
 
-from src.Track import Line, Circle, BezierSpline
+from src.Track import Line, BezierSpline
 from src.Track import Track
 
 class TestTrackArcLengthToLambda(unittest.TestCase):
     """
-
+    Testing map from arc length to segment and lambda parameter in Track
     """
 
     def setUp(self):
@@ -15,13 +14,22 @@ class TestTrackArcLengthToLambda(unittest.TestCase):
         self.line_2 = Line.Line([[2, 0, 0], [2, 2, 0]])
         self.line_3 = Line.Line([[2, 2, 0], [0, 2, 0]])
 
-        self.track = Track.Track([self.line_1, self.line_2, self.line_3])
+        self.line_track = Track.Track([self.line_1, self.line_2, self.line_3])
 
         # Two Bezier lines track setup
+        knots_1 = [[0, 0, 0], [12, 0, 0]]
+        control_points_1 = [[4, 0, 0], [8, 0, 0]]
+        self.bezier_1 = BezierSpline.CubicBezier(knots_1, control_points_1)
+
+        knots_2 = [[12, 0, 0], [12, 12, 0]]
+        control_points_2 = [[12, 4, 0], [12, 8, 0]]
+        self.bezier_2 = BezierSpline.CubicBezier(knots_2, control_points_2)
+
+        self.bezier_track = Track.Track([self.bezier_1, self.bezier_2])
 
     def test_three_lines_arc_length_1(self):
 
-        segment, lambda_parameter = self.track.segment_lambda_from_arc_length(1)
+        segment, lambda_parameter = self.line_track.segment_lambda_from_arc_length(1)
 
         # Test lambda
         self.assertAlmostEqual(lambda_parameter, 0.5)
@@ -32,7 +40,7 @@ class TestTrackArcLengthToLambda(unittest.TestCase):
 
     def test_three_lines_arc_length_1p5(self):
 
-        segment, lambda_parameter = self.track.segment_lambda_from_arc_length(1.5)
+        segment, lambda_parameter = self.line_track.segment_lambda_from_arc_length(1.5)
 
         # Test lambda
         self.assertAlmostEqual(lambda_parameter, 0.75)
@@ -43,7 +51,7 @@ class TestTrackArcLengthToLambda(unittest.TestCase):
 
     def test_three_lines_arc_length_2p5(self):
 
-        segment, lambda_parameter = self.track.segment_lambda_from_arc_length(2.5)
+        segment, lambda_parameter = self.line_track.segment_lambda_from_arc_length(2.5)
 
         # Test lambda
         self.assertAlmostEqual(lambda_parameter, 0.25)
@@ -54,7 +62,7 @@ class TestTrackArcLengthToLambda(unittest.TestCase):
 
     def test_three_lines_arc_length_5(self):
 
-        segment, lambda_parameter = self.track.segment_lambda_from_arc_length(5)
+        segment, lambda_parameter = self.line_track.segment_lambda_from_arc_length(5)
 
         # Test lambda
         self.assertAlmostEqual(lambda_parameter, 0.5)
@@ -63,8 +71,38 @@ class TestTrackArcLengthToLambda(unittest.TestCase):
         truth = (self.line_3.coordinates == segment.coordinates)
         self.assertEqual(truth, True)
 
+    def test_two_bezier_arc_length_6(self):
 
+        segment, lambda_parameter = self.bezier_track.segment_lambda_from_arc_length(6)
 
+        # Test lambda
+        self.assertAlmostEqual(lambda_parameter, 0.5)
+
+        # Test correct segment
+        truth = (self.bezier_1.knots == segment.knots)
+        self.assertEqual(truth, True)
+
+    def test_two_bezier_arc_length_11(self):
+
+        segment, lambda_parameter = self.bezier_track.segment_lambda_from_arc_length(11)
+
+        # Test lambda
+        self.assertAlmostEqual(lambda_parameter, 11./12.)
+
+        # Test correct segment
+        truth = (self.bezier_1.knots == segment.knots)
+        self.assertEqual(truth, True)
+
+    def test_two_bezier_arc_length_13(self):
+
+        segment, lambda_parameter = self.bezier_track.segment_lambda_from_arc_length(13)
+
+        # Test lambda
+        self.assertAlmostEqual(lambda_parameter, 1./12.)
+
+        # Test correct segment
+        truth = (self.bezier_2.knots == segment.knots)
+        self.assertEqual(truth, True)
 
 
 if __name__ == '__main__':
